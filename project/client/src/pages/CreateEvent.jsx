04f53d503d199b1440c15d9c6744076cbf6f9d59
sofiya thamanna <sofiyathamanna786@gmail.com>
2025-06-22 20:30:27 +0530

    updated

--- project/client/src/pages/CreateEvent.jsx
+++ project/client/src/pages/CreateEvent.jsx
@@ -1,18 +1,33 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import EventForm from "../components/EventForm";
+import { useAuth } from "../context/AuthContext"; 
 
 function CreateEvent() {
-    const navigate = useNavigate();
-    const handleAdd = (event) => {
-        const existing = JSON.parse(localStorage.getItem("events")) || [];
-        const updated = [...existing, event];
-        localStorage.setItem("events", JSON.stringify(updated));
-        navigate("/");
+  const navigate = useNavigate();
+  const { user } = useAuth(); 
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login", { state: { from: "/create" } });
     }
-    return (
-        <div>
-            <EventForm onAdd={handleAdd} />
-        </div>
-    )
+  }, [user, navigate]);
+
+  const handleAdd = (event) => {
+    const existing = JSON.parse(localStorage.getItem("events")) || [];
+    const updated = [...existing, event];
+    localStorage.setItem("events", JSON.stringify(updated));
+    navigate("/");
+  };
+
+
+  if (!user) return null;
+
+  return (
+    <div>
+      <EventForm onAdd={handleAdd} />
+    </div>
+  );
 }
+
 export default CreateEvent;
